0cf94a278e55f76212137a0c1340ab7f4f553d33
Shailendra Prajapati <skp1621@gmail.com>
2023-05-26 13:47:12 +0530

    Update file_func.py

--- file_func.py
+++ file_func.py
@@ -5,20 +5,21 @@ import joblib
 
 # function to create temporary csv data file from it's binary object
 def create_file(file_name, data_bytes):
-    with open("tmp_files/" + file_name, 'wb') as f:
+    with open("tmp_files/"+file_name, 'wb') as f:
         f.write(data_bytes)
 
 
 # delete temporary files
 def delete_tmp_files(folder_name):
-    folder = folder_name
-    for filename in os.listdir(folder):
-
-        file_path = os.path.join(folder, filename)
-        try:
-            os.unlink(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+        folder = folder_name
+        for filename in os.listdir(folder):
+            
+            file_path = os.path.join(folder, filename)
+            try:
+                os.unlink(file_path)
+    
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
 # function to perform transformations on the data
